84974cb0327793d24685ca558e7935cb386ac568
adoyeong <puppy2621235@korea.ac.kr>
2023-03-15 10:41:42 -0400

    Add new main.cpp

--- main.cpp
+++ main.cpp
@@ -13,21 +13,21 @@ int main(int argc, char*argv[])
 
 	uint32_t a = 0, b = 0, result = 0;
 
-	a = buffer1[0];
+	a = buffer1[1];
 	a = a << 8;
-	a = a | buffer1[1];
-	a = a << 8;
-	a = a | buffer1[2];
+	a = a | buffer1[0];
 	a = a << 8;
 	a = a | buffer1[3];
+	a = a << 8;
+	a = a | buffer1[2];
 
-	b = buffer2[0];
+	b = buffer2[1];
 	b = b << 8;
-	b = b | buffer2[1];
-	b = b << 8;
-	b = b | buffer2[2];
+	b = b | buffer2[0];
 	b = b << 8;
 	b = b | buffer2[3];
+	b = b << 8;
+	b = b | buffer2[2];
 
 	result = a + b;
 	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", a, a, b, b, result, result);
